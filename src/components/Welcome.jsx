23aeec88b81093e01164e09a919aaa05fd359f7e
DanyGlez94 <daniel_glez94@hotmail.com>
2022-03-24 12:10:19 -0600

    Connection with metamask wallet completed

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,8 @@
+import { useContext } from 'react';
 import { AiFillPlayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
-
+import { TransactionContext } from '../context/TransactionContext';
 import { Loader } from './';
 
 const commonStyles = 'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
@@ -18,11 +19,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-    const connectWallet = () => {
-        // return (
-        //     <div>Welcome</div>
-        // )
-    }
+    const { connectWallet, currentAccount } = useContext(TransactionContext);
 
     const handleSubmit = () => {
         // return (
@@ -40,13 +37,13 @@ const Welcome = () => {
                     <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
                         Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
                     </p>
-                    <button
+                    {!currentAccount && <button
                         type='button'
                         onClick={connectWallet}
                         className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
                     >
                         <p className='text-white text-base font-semibold'>Connect Wallet</p>
-                    </button>
+                    </button>}
                     <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
                             Reliability
